7e3150d7a40509de3cc8a3de505a75eb74d69cef
Volodymyr Protsenko <vovapro01@gmail.com>
2020-07-05 18:27:48 +0300

    first commit

---
+++ src/test/java/boot/springbootaplication/SpringBootAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.springbootaplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
