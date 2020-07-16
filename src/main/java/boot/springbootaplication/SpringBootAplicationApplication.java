0e2cdd665b6f3cc7c05a2ae8aea47c0603e80786
Volodymyr Protsenko <vovapro01@gmail.com>
2020-07-16 16:58:31 +0300

    removed unnecessary hard testing

--- src/main/java/boot/springbootaplication/SpringBootAplicationApplication.java
+++ src/main/java/boot/springbootaplication/SpringBootAplicationApplication.java
@@ -11,12 +11,5 @@ public class SpringBootAplicationApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(SpringBootAplicationApplication.class, args);
-        User user = new User();
-        UserService userService = run.getBean(UserService.class);
-        user.setIdFromFile("AD1SAD5");
-        user.setProfileName("Vasa");
-        user.setPassword("1234");
-        userService.save(user);
-        System.out.println("User was added to db");
     }
 }
