ab8ec469cacbe99a0025c5427d1f130f1893f35f
Volodymyr Protsenko <vovapro01@gmail.com>
2020-07-16 17:12:17 +0300

    CheckStyle fix

--- src/main/java/boot/springbootaplication/SpringBootAplicationApplication.java
+++ src/main/java/boot/springbootaplication/SpringBootAplicationApplication.java
@@ -1,15 +1,12 @@
 package boot.springbootaplication;
 
-import boot.springbootaplication.model.User;
-import boot.springbootaplication.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootAplicationApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(SpringBootAplicationApplication.class, args);
+        SpringApplication.run(SpringBootAplicationApplication.class, args);
     }
 }
