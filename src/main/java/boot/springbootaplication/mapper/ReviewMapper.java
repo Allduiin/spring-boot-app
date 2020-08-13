c3f802a26ac1b1f5969f97a586d80423033d00f5
Volodymyr Protsenko <vovapro01@gmail.com>
2020-08-13 08:53:54 +0300

    Cherry pick: Added simple review controller

--- src/main/java/boot/springbootaplication/mapper/ReviewMapper.java
+++ src/main/java/boot/springbootaplication/mapper/ReviewMapper.java
@@ -1,28 +1,49 @@
 package boot.springbootaplication.mapper;
 
 import boot.springbootaplication.model.Review;
-import boot.springbootaplication.model.dto.ReviewFromFileDto;
+import boot.springbootaplication.model.dto.ReviewRequestDto;
+import boot.springbootaplication.model.dto.ReviewResponseDto;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ReviewMapper {
-    public List<Review> getReviewFromDto(List<ReviewFromFileDto> reviews) {
+
+    public List<Review> getReviewsFromDto(List<ReviewRequestDto> reviews) {
         List<Review> result = new ArrayList<>();
-        for (ReviewFromFileDto dto : reviews) {
-            Review review = new Review();
-            review.setProductId(dto.getProductId());
-            review.setUserId(dto.getUserId());
-            review.setProfileName(dto.getProfileName());
-            review.setHelpfulnessNumerator(dto.getHelpfulnessNumerator());
-            review.setHelpfulnessDenominator(dto.getHelpfulnessDenominator());
-            review.setScore(dto.getScore());
-            review.setTime(dto.getTime());
-            review.setSummary(dto.getSummary());
-            review.setText(dto.getText());
-            result.add(review);
+        for (ReviewRequestDto dto : reviews) {
+            result.add(getReviewFromReviewRequestDto(dto));
         }
         return result;
+    }
+
+    public Review getReviewFromReviewRequestDto(ReviewRequestDto dto) {
+        Review review = new Review();
+        review.setProductId(dto.getProductId());
+        review.setUserId(dto.getUserId());
+        review.setProfileName(dto.getProfileName());
+        review.setHelpfulnessNumerator(dto.getHelpfulnessNumerator());
+        review.setHelpfulnessDenominator(dto.getHelpfulnessDenominator());
+        review.setScore(dto.getScore());
+        review.setTime(dto.getTime());
+        review.setSummary(dto.getSummary());
+        review.setText(dto.getText());
+        return review;
+    }
+
+    public ReviewResponseDto getReviewResponseDtoFromReview(Review review) {
+        ReviewResponseDto dto = new ReviewResponseDto();
+        dto.setId(review.getId());
+        dto.setProductId(review.getProductId());
+        dto.setUserId(review.getUserId());
+        dto.setProfileName(review.getProfileName());
+        dto.setHelpfulnessNumerator(review.getHelpfulnessNumerator());
+        dto.setHelpfulnessDenominator(review.getHelpfulnessDenominator());
+        dto.setScore(review.getScore());
+        dto.setTime(review.getTime());
+        dto.setSummary(review.getSummary());
+        dto.setText(review.getText());
+        return dto;
     }
 }
