a0f8189be41d63a71f730ac81ce98ec9232532a6
Volodymyr Protsenko <vovapro01@gmail.com>
2020-08-14 14:28:41 +0300

    Added UserController, changed save at UserService, have a problem with adding new reviews to existed user

---
+++ src/main/java/boot/springbootaplication/model/dto/UserResponseDto.java
@@ -0,0 +1,11 @@
+package boot.springbootaplication.model.dto;
+
+import boot.springbootaplication.model.Review;
+import boot.springbootaplication.model.Role;
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class UserResponseDto {
+    private String profileName;
+}
